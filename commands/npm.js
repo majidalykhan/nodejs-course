456be7fc1b5cd8555d302b4aa2913d55ca2bd695
majidalykhan <majidalykhan.com>
2022-04-26 00:20:50 +0500

    Worked with npm and package.json

---
+++ commands/npm.js
@@ -0,0 +1,13 @@
+//npm - global command, comes with node
+//npm --version
+
+//local dependency - use it only in this project
+//npm i <packageName>
+
+//global dependency - use it in any project
+//npm install -g <packageName>
+
+//package.json - manifest file (stores important info about project or package)
+//manual installation - create package.json in root, create properties etc.
+//npm init (step by step, enter to skip)
+//npm init -y (everything default)
--- http.js
+++ http.js
@@ -16,3 +16,4 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(5000);
+
