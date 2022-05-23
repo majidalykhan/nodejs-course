c57fd59297cd12c710fb8027357621640e975373
majidalykhan <majidalykhan.com>
2022-05-23 16:21:07 +0500

    Http headers

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("User hit the server");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1> Home Page </h1>");
+  res.end();
 });
 
 server.listen(5000);
