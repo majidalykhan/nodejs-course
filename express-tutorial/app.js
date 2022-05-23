888c38a826be93ba3b556e13de5b46890e0c114c
majidalykhan <majidalykhan.com>
2022-05-23 16:36:09 +0500

    Http html file

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,25 +1,25 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
-  
   const url = req.url;
 
-  if(url === '/'){
+  //home page
+  if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1> Home Page </h1>");
+    res.write(homePage);
     res.end();
-  }
-  else if(url ==='/about'){
+  } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1> About </h1>");
     res.end();
-  }
-  else {
+  } else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1> Page not found </h1>");
     res.end();
   }
-  
 });
 
 server.listen(5000);
