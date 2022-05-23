5e29cdd05e9b3f42d4eea4682ea0ec2a7c4d0216
majidalykhan <majidalykhan.com>
2022-05-23 16:49:01 +0500

    Http Navbar example

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const { readFileSync } = require("fs");
 
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -10,6 +13,21 @@ const server = http.createServer((req, res) => {
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
+    res.end();
+  //styles
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+    //Image
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+    //Logic
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/js" });
+    res.write(homeLogic);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
