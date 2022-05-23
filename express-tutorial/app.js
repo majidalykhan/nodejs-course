8b02bf47c3ef8004b54dbd71231b78263a90db6b
majidalykhan <majidalykhan.com>
2022-05-24 02:15:36 +0500

    Express - App Example

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
+//Setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
