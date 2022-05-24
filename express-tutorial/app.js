bf5ab3ad85cca82305ef2ce946f8ee3f3b3ca60e
majidalykhan <majidalykhan.com>
2022-05-24 22:34:55 +0500

    Methods - POST (Form example)

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -4,8 +4,24 @@ const app = express();
 
 let { people } = require("./data");
 
+//Static assets
+app.use(express.static("./methods-public"));
+
+//Parse form data
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/people", (req, res) => {
   res.json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send("Please provide credentials");
 });
 
 app.listen(5000, () => {
