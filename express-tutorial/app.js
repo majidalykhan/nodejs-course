35e66ff4242ab2ad18ff2abf23f6a005d0853066
majidalykhan <majidalykhan@gmail.com>
2022-05-24 22:54:41 +0500

    Methods - POST (Javacript Example)

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -10,10 +10,27 @@ app.use(express.static("./methods-public"));
 //Parse form data
 app.use(express.urlencoded({ extended: false }));
 
+//Parse json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.json({ success: true, data: people });
 });
 
+//POST - Javascript example
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+
+  res.status(201).json({ success: true, person: name });
+});
+
+//POST - Form Example
 app.post("/login", (req, res) => {
   const { name } = req.body;
 
