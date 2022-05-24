54ad1ce4f0784f4fb100f875163289b8d6e522cc
majidalykhan <majidalykhan@gmail.com>
2022-05-24 23:18:56 +0500

    Methods - PUT

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -30,6 +30,19 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ success: true, person: name });
 });
 
+//Postman
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
 //POST - Form Example
 app.post("/login", (req, res) => {
   const { name } = req.body;
@@ -39,6 +52,29 @@ app.post("/login", (req, res) => {
   }
 
   res.status(401).send("Please provide credentials");
+});
+
+// PUT
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with ID: ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
