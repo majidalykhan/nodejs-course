66ffa2adb71626201a6470a6cc0ac11379ae47dc
majidalykhan <majidalykhan.com>
2022-05-24 16:25:16 +0500

    Route parameters - getting single product

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -8,13 +8,30 @@ app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">Products</a> ');
 });
 
+//All products
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
-    return {id, name, image};
+    return { id, name, image };
   });
 
   res.send(newProducts);
+});
+
+//Single product -- route params
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+
+  if (!singleProduct) {
+    res.status(404).send("Product does not exist");
+  }
+
+  res.json(singleProduct);
 });
 
 app.listen(5000, () => {
